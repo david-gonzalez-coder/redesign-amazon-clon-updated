d4c2d417923f1b23662daa65eae5d58f7c924f7a
david-gonzalez-coder <david-gonzalez-admin@protonmail.com>
2023-04-28 20:48:06 -0600

    responsive added to Menu

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,23 +1,20 @@
-import { Outlet } from "react-router"
-import {Menu} from "./index"
-import styled from "styled-components"
 
-const Wrapper = styled.div`
-    display: grid ;
-    grid-template-columns: 80px 1fr ;
-`
+import { Outlet, } from "react-router"
+
+import {Menu} from "./index"
+
 
 const Layout = () => {
     
     return (
-        <Wrapper className="w-full min-h-screen ">
+        <div className="gap-4 sm:gap-0 grid-rows-[80px,1fr] sm:grid-rows-1 grid-cols-1 sm:grid-cols-[80px,1fr] grid grid w-full min-h-screen ">
             <Menu />
             <div></div>
             <div>
                 <Outlet />
             </div>
             
-        </Wrapper>
+        </div>
     )
 }
 export default Layout
--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,36 +1,59 @@
 import styled from "styled-components"
-import {useState} from "react"
+import {useEffect, useState} from "react"
 import { Link } from "react-router-dom"
+import { useLocation } from "react-router"
 //icons
 import { BsSearch } from 'react-icons/bs'
 import { BiCart, BiUser } from 'react-icons/bi'
-import { RiLayoutGridFill } from 'react-icons/ri'
 import { MdAccountCircle } from 'react-icons/md'
 import {HiOutlineMenuAlt1} from "react-icons/hi"
 //images
 import amazonLogo from '../assets/images/amazonLogo.png'
 
-
 const Drawer = styled.header`
-    grid-area: menu ;
-    width: ${({open})=> open ? "300px" : "80px"} ;
+    height: ${({open})=> open ? "100vh" : "80px"} ;
+    nav{
+        display: ${({open})=> open ? "flex" : "none"} ;
+        visibility: ${({open})=> open ? "visible" : "hidden"} ;
+        flex-grow: 1;
+    }
+    
+    @media (min-width: 640px){
+        width: ${({open})=> open ? "300px" : "80px"} ;
+        height: 100vh ;
+        nav{
+            visibility:visible ;
+            display: flex ;
+        }
+    }
+    
 `
 const Menu = () => {
     const [open, setOpen] = useState(false)
+    const location = useLocation()
+    useEffect(()=>{
+        setOpen(false)
+    },[location])
     return (
-        <Drawer open={open} className="h-screen fixed rounded-r-xl z-50 transition-all bg-white  flex flex-col items-center justify-between p-4 sm:p-8">
-            <Link to="/"className="inline-block w-[60px] aspect-square">
-                <img src={amazonLogo} alt="logo" className="w-full h-full object-containt" /> 
-            </Link>
-            <nav>
-                <Link to="search"className="flex items-center my-8 gap-4 text-lg"><BsSearch className="text-[1.6rem]"/> {open ? "Search" : ""}</Link>
-                {/* <Link to="deparments"className="flex items-center my-8 gap-4 text-lg"><RiLayoutGridFill className="text-[1.6rem]"/> {open ? "Deparments" : ""}</Link> */}
-                <Link to="basket"className="flex items-center my-8 gap-4 text-lg"><BiCart className="text-[1.6rem]"/> {open ? "Basket" : ""}</Link>
-                <Link to="sign-in"className="flex items-center my-8 gap-4 text-lg"><BiUser className="text-[1.6rem]"/> {open ? "Sign in" : ""}</Link>
-                <Link to="sign-up"className="flex items-center my-8 gap-4 text-lg"><MdAccountCircle className="text-[1.6rem]"/> {open ? "Sign up" : ""}</Link>
-            </nav>
-            <button onClick={()=>setOpen(!open)}><HiOutlineMenuAlt1 className="text-[2rem]"/></button>
-        </Drawer>
+        <>
+            <Drawer open={open} className="flex w-full sm:h-screen fixed rounded-r-xl z-50 transition-all bg-white flex-col items-center justify-between p-4 sm:p-8">
+                <div className="flex sm:block w-full sm:w-fit items-center justify-between">
+                    <Link to="/"className="inline-block w-[60px] aspect-square">
+                        <img src={amazonLogo} alt="logo" className="w-full h-full object-containt" /> 
+                    </Link>
+                    <button className="inline sm:hidden" onClick={()=>setOpen(!open)}><HiOutlineMenuAlt1 className="text-[2rem]"/></button>
+                </div>
+                
+                <nav className="flex items-center flex-col justify-center">
+                    <Link to="search"className="flex items-center my-5 gap-4 text-lg"><BsSearch className="text-[1.6rem]"/> {open ? "Search" : ""}</Link>
+                    <Link to="basket"className="flex items-center my-5 gap-4 text-lg"><BiCart className="text-[1.6rem]"/> {open ? "Basket" : ""}</Link>
+                    <Link to="sign-in"className="flex items-center my-5 gap-4 text-lg"><BiUser className="text-[1.6rem]"/> {open ? "Sign in" : ""}</Link>
+                    <Link to="sign-up"className="flex items-center my-5 gap-4 text-lg"><MdAccountCircle className="text-[1.6rem]"/> {open ? "Sign up" : ""}</Link>
+                </nav>
+                <button className="hidden sm:inline" onClick={()=>setOpen(!open)}><HiOutlineMenuAlt1 className="text-[2rem]"/></button>
+            </Drawer>
+        </>
+        
     )
 }
 export default Menu
